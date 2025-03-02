44bbc608fa4fe136596314de6c13cede61758d52
dhirajnitk <dhirajnitk@gmail.com>
2025-03-03 00:54:05 +0100

    Delete copy files

--- src/main/java/se/nexus/interview/radius/server/ResponsePacket.java
+++
@@ -1,8 +0,0 @@
-package se.nexus.interview.radius.server;
-
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-
-public interface ResponsePacket {
-    byte[] build() throws NoSuchAlgorithmException, IOException;
-}
